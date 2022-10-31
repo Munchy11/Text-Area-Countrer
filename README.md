4e7aa3c4260ebd9fb603906b055abc21cb2a5eaf
Lucas Muenchrath <lucasmuenchrath@Lucass-MacBook-Pro.local>
2022-10-31 00:33:51 -0600

    achiev #1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CPNT-262-RND2
+
+### - code attributes: https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+
+https://gist.github.com/acidtone/74727a562940ead812f46c1b1b870d19
---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Text Area Counter //
+
+let textArea = document.getElementById("textbox");
+let characterCounter = document.getElementById("text-count");
+const maxNumOfChars = 50;
+
+const countCharacters = () => {
+        let numOfEnteredChars = textArea.value.length;
+    let counter = maxNumOfChars - numOfEnteredChars;
+    characterCounter.textContent = counter + "/50";
+
+  
+};
+
+textArea.addEventListener("input", countCharacters);
